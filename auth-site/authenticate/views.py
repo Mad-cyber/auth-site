d11cef45caa1b36f6fe0307974345c08e2830494
Mad-cyber <msker.ryan@gmail.com>
2023-05-01 12:24:02 +0100

    add user registeration and update password pages

--- auth-site/authenticate/views.py
+++ auth-site/authenticate/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib import messages
+from .forms import SignUpForm, EditProfileForm
 
 
 def home(request):
@@ -16,7 +17,8 @@ def login_user(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, ('Welcome back you have been logged in!'))
+                messages.success(
+                    request, ('Welcome back you have been logged in!'))
                 return redirect('home')
             else:
                 messages.success(
@@ -25,25 +27,61 @@ def login_user(request):
         return redirect('loginpage')
     else:
         return render(request, 'authenicate/login.html', {})
-    
+
+
 def logout_user(request):
     logout(request)
     messages.success(request, ('You have logged out sucessfully'))
-    return redirect ('home')
+    return redirect('home')
+
 
 def register_user(request):
-     if request.method == 'POST':
-         form = UserCreationForm(request.POST)
-         if form.is_valid():
-             form.save()
-             username = form.cleaned_data['username']
-             password = form.cleaned_data['password1']
-             user = authenticate(request, username=username, password=password)
-             login(request, user)
-             messages.success(request, ('You are now registered'))
-             return redirect('home')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You are now registered'))
+            return redirect('home')
 
-     else:
-         form = UserCreationForm()
-     context = {'form': form}    
-     return render(request, 'authenicate/register.html', context)
+    else:
+        form = SignUpForm()
+    context = {'form': form}
+    return render(request, 'authenicate/register.html', context)
+
+
+def edit_profile(request):
+    form = None  # define form variable and set it to None
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You have updated your profile'))
+            return redirect('home')
+    else:
+        form = EditProfileForm(instance=request.user)
+    context = {'form': form}
+    return render(request, 'authenicate/edit_profile.html', context)
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Your password was successfully updated!')
+            update_session_auth_hash(request, user)  # Important!
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please try again.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'authenicate/change_password.html', {'form': form})
+
+
